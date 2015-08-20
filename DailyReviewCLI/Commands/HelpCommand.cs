2492fd1472ddf76a0c2f6555c7e7dbee84968d46
nergal-perm <nergal@psu.ru>
2015-08-20 13:53:33 +0500

    chronodex drawing procedures (not finished)

--- DailyReviewCLI/Commands/HelpCommand.cs
+++ DailyReviewCLI/Commands/HelpCommand.cs
@@ -7,58 +7,23 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.IO;
-using System.Reflection;
+using DailyReviewCLI.Utils;
 
 namespace DailyReviewCLI.Commands {
 	/// <summary>
 	/// Description of HelpCommand.
 	/// </summary>
 	public class HelpCommand : IRunnable {
-		public HelpCommand() {
-		}
 
 		#region IRunnable implementation
 
 		public void run() {
-			Assembly myAssembly = Assembly.GetExecutingAssembly();
-			Stream myStream = myAssembly.GetManifestResourceStream("Chronodex");
-			Image myImage = Image.FromStream(myStream);
-			myStream.Close();
-			Graphics go = Graphics.FromImage(myImage);
-			
-			var rectInner = new Rectangle(471, 365, 288, 288);
-			var rectFirst = new Rectangle(415, 309, 400, 400);
-			var rectSecond = new Rectangle(355, 249, 519, 520);
-			var rectOuter = new Rectangle(298, 192, 634, 634);
-
-//			GraphicsPath gp = new GraphicsPath();
-//			gp.AddArc(rectOuter, 0, 45);	
-//			gp.AddArc(rectInner, 0, 45);
-//			gp.AddLine(715.8233f, 610.8233f, 838.2993f, 732.2992f);
-//			gp.AddLine(757.9999f, 509f, 930.9999f, 509f);
-//			
-//			foreach (var point in gp.PathPoints) {
-//				Console.WriteLine(point.ToString());
-//			}
-//			go.DrawLine(new Pen(Color.Blue, 4), 715.8233f, 610.8233f, 838.2993f, 732.2992f);
-//			go.DrawLine(new Pen(Color.Red, 4), 757.9999f, 509f, 930.9999f, 509f);
-//			go.FillPath(Brushes.Aqua, gp);
-			
-			go.DrawEllipse(new Pen(Color.Red, 1), rectOuter);
-			go.DrawEllipse(new Pen(Color.Blue, 1), rectSecond);
-			go.DrawEllipse(new Pen(Color.Green, 1), rectFirst);
-			go.DrawEllipse(new Pen(Color.Yellow, 1), rectInner);
-			
-			
-			myImage.Save(@"D:\Temp\test.png");
-			
-			myImage.Dispose();
-			go.Dispose();
+			Chronodex chr = new Chronodex();
+			chr.DrawChronodexSector("1215", 3, FocusLevel.Normal);
+			chr.DrawChronodexSector("1630", 6, FocusLevel.High);
+			chr.Dispose();
 		}
-
 		#endregion
+		
 	}
 }
---
+++ DailyReviewCLI/Utils/Chronodex.cs
@@ -0,0 +1,182 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: terekhov-ev
+ * Date: 20.08.2015
+ * Time: 13:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Reflection;
+
+namespace DailyReviewCLI.Utils {
+	/// <summary>
+	/// Description of Chronodex.
+	/// </summary>
+	public class Chronodex : IDisposable {
+		private Dictionary<string, float> angles;
+		private readonly Rectangle[] levels = {
+			new Rectangle(471, 365, 288, 288),
+			new Rectangle(415, 309, 400, 400),
+			new Rectangle(355, 249, 519, 520),
+			new Rectangle(298, 192, 634, 634)
+		};
+		private Graphics go;
+		private Image myImage;
+			
+		public Chronodex() {
+			initializeAnglesDictionary();
+			initializeGraphicsObject();
+
+		}
+
+		void initializeGraphicsObject() {
+			Assembly myAssembly = Assembly.GetExecutingAssembly();
+			Stream myStream = myAssembly.GetManifestResourceStream("Chronodex");
+			myImage = Image.FromStream(myStream);
+			myStream.Close();
+			go = Graphics.FromImage(myImage);
+		}
+		
+		
+		
+		public void DrawChronodexSector(string startTime, int duration, FocusLevel lvl) {
+
+			
+			GraphicsPath gp = new GraphicsPath();
+			gp.AddArc(levels[(int)lvl], angles[startTime], duration * 7.5f);
+			gp.AddArc(levels[0], angles[startTime], duration * 7.5f);
+			gp.AddLine(gp.PathPoints[0], gp.PathPoints[4]);
+			gp.AddLine(gp.PathPoints[3], gp.PathPoints[7]);
+			
+			HatchBrush brush = new HatchBrush(HatchStyle.DarkDownwardDiagonal, Color.BlueViolet, Color.Transparent);
+			go.FillPath(brush, gp);
+			gp.Dispose();			
+
+
+		}
+
+		void initializeAnglesDictionary() {
+			angles = new Dictionary<string, float>();
+			
+			angles.Add("0000", 270.0f);
+			angles.Add("0015", 277.5f);
+			angles.Add("0030", 285.0f);
+			angles.Add("0045", 292.5f);
+			angles.Add("0100", 300.0f);
+			angles.Add("0115", 307.5f);
+			angles.Add("0130", 315.0f);
+			angles.Add("0145", 322.5f);
+			angles.Add("0200", 330.0f);
+			angles.Add("0215", 337.5f);
+			angles.Add("0230", 345.0f);
+			angles.Add("0245", 352.5f);
+			angles.Add("0300", 0.0f);
+			angles.Add("0315", 7.5f);
+			angles.Add("0330", 15.0f);
+			angles.Add("0345", 22.5f);
+			angles.Add("0400", 30.0f);
+			angles.Add("0415", 37.5f);
+			angles.Add("0430", 45.0f);
+			angles.Add("0445", 52.5f);
+			angles.Add("0500", 60.0f);
+			angles.Add("0515", 67.5f);
+			angles.Add("0530", 75.0f);
+			angles.Add("0545", 82.5f);
+			angles.Add("0600", 90.0f);
+			angles.Add("0615", 97.5f);
+			angles.Add("0630", 105.0f);
+			angles.Add("0645", 112.5f);
+			angles.Add("0700", 120.0f);
+			angles.Add("0715", 127.5f);
+			angles.Add("0730", 135.0f);
+			angles.Add("0745", 142.5f);
+			angles.Add("0800", 150.0f);
+			angles.Add("0815", 157.5f);
+			angles.Add("0830", 165.0f);
+			angles.Add("0845", 172.5f);
+			angles.Add("0900", 180.0f);
+			angles.Add("0915", 187.5f);
+			angles.Add("0930", 195.0f);
+			angles.Add("0945", 202.5f);
+			angles.Add("1000", 210.0f);
+			angles.Add("1015", 217.5f);
+			angles.Add("1030", 225.0f);
+			angles.Add("1045", 232.5f);
+			angles.Add("1100", 240.0f);
+			angles.Add("1115", 247.5f);
+			angles.Add("1130", 255.0f);
+			angles.Add("1145", 262.5f);
+			angles.Add("1200", 270.0f);
+			angles.Add("1215", 277.5f);
+			angles.Add("1230", 285.0f);
+			angles.Add("1245", 292.5f);
+			angles.Add("1300", 300.0f);
+			angles.Add("1315", 307.5f);
+			angles.Add("1330", 315.0f);
+			angles.Add("1345", 322.5f);
+			angles.Add("1400", 330.0f);
+			angles.Add("1415", 337.5f);
+			angles.Add("1430", 345.0f);
+			angles.Add("1445", 352.5f);
+			angles.Add("1500", 0.0f);
+			angles.Add("1515", 7.5f);
+			angles.Add("1530", 15.0f);
+			angles.Add("1545", 22.5f);
+			angles.Add("1600", 30.0f);
+			angles.Add("1615", 37.5f);
+			angles.Add("1630", 45.0f);
+			angles.Add("1645", 52.5f);
+			angles.Add("1700", 60.0f);
+			angles.Add("1715", 67.5f);
+			angles.Add("1730", 75.0f);
+			angles.Add("1745", 82.5f);
+			angles.Add("1800", 90.0f);
+			angles.Add("1815", 97.5f);
+			angles.Add("1830", 105.0f);
+			angles.Add("1845", 112.5f);
+			angles.Add("1900", 120.0f);
+			angles.Add("1915", 127.5f);
+			angles.Add("1930", 135.0f);
+			angles.Add("1945", 142.5f);
+			angles.Add("2000", 150.0f);
+			angles.Add("2015", 157.5f);
+			angles.Add("2030", 165.0f);
+			angles.Add("2045", 172.5f);
+			angles.Add("2100", 180.0f);
+			angles.Add("2115", 187.5f);
+			angles.Add("2130", 195.0f);
+			angles.Add("2145", 202.5f);
+			angles.Add("2200", 210.0f);
+			angles.Add("2215", 217.5f);
+			angles.Add("2230", 225.0f);
+			angles.Add("2245", 232.5f);
+			angles.Add("2300", 240.0f);
+			angles.Add("2315", 247.5f);
+			angles.Add("2330", 255.0f);
+			angles.Add("2345", 262.5f);
+		}
+
+		#region IDisposable implementation
+
+		public void Dispose() {
+			myImage.Save(@"D:\Temp\test.png");
+			
+			myImage.Dispose();
+			go.Dispose();	
+		}
+
+		#endregion
+	}
+	
+	
+	public enum FocusLevel {
+		Low = 1,
+		Normal = 2,
+		High = 3
+	}
+}
